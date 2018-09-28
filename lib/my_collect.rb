778726cff9361bbe7f03ca6dac232ed2cc7026d7
Virginia Dooley <vcdooley@gmail.com>
2018-09-28 11:34:15 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def my_collect(languages, empty_array)
   x = 0 
   my_collect(empty_array) do |x|
     while x < languages.length
-    yield(languages[x])
-  end
+      yield(languages[x])
+    end
     my_collect
+  end
 end
