40be2bd7545bd7bca77aa0755e7ca396b6b03f58
Virginia Dooley <vcdooley@gmail.com>
2018-09-28 12:00:55 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,5 +2,6 @@ def my_collect(array)
   i = 0 
   empty_array = []
   while i < array.length
-  empty_array << yield(array[i])
+  empty_array << yield(array[i]) #pushes each element of array into new empty_array
+  i += 1 #counter
 end
